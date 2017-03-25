ea373366455648e4d547b11ea8e5cecbb3ae1ecd
franksco <franksco39@gmail.com>
2017-03-25 11:11:38 -0700

    completed RandomWordGen func and added facts to our array

--- RandoFacto/ViewController.swift
+++ RandoFacto/ViewController.swift
@@ -27,11 +27,24 @@ class ViewController: UIViewController {
 
     @IBAction func RandomFact(_ sender: Any) {
         
-        let array = ["", "", "", "", "", "", "", "", "", "", "", ""]
+        let array = ["The Rancor from smash-hit movie saga Star Wars was based on a cross between a bear and a potato.",
+                     "Out of the millions of creatures on Earth, humans are only 1 of 3 species capable of laughter, the other 2 being chimpanzees and rats.",
+                     "In Sweden, every time a Blood donor’s blood is used to save a life a text message is sent to them.",
+                     "As part of a contract, J.K. Rowling (the author of the Harry Potter series) can go to Universal Studios and if anything is deemed incorrect, inaccurate, or she doesn’t like it she can get it changed.",
+                     "A Mumbai thief was force-fed 48 bananas after being caught stealing a gold chain, the police ordered in the 48 piece basket and waited until the criminal could excrete the chain.",
+                     "The late Steve Irwin and his whole film crew stopped filming in 2003 to aid in a 2 day search and rescue of missing divers.",
+                     "In six hours, the world’s deserts receive more energy from the sun than humans consume in a year.",
+                     "Hitler’s brother; Patrick William Hitler, lived in Liverpool. Patrick’s son later joined the US army during WW2.",
+                     "If an object, such as stars or comets, goes to close to a black hole it will be stretched like a membrane, this is known as ‘spaghettification’.",
+                     "A plane passenger once played a pirated movie during a flight, only to discover the lead actress Kriti Sanon sat beside him.",
+                     "Samsung is also a full time weapons manufacturer, who apparently uses the most up to date technology to support world peace and security. And we all thought the S7 was impressive.",
+                     "The human eye is so sensitive that, if the Earth were flat and it was a dark night, a candle’s flame could be seen from 30 miles away."]
         
-        let RandomFact = Int(arc4random_uniform(UInt32(array.count)))
-        label.text = array[RandomFact]
+        let RandomFact = Int(arc4random_uniform(UInt32(array.count))) //Our code to count through the array
+        label.text = array[RandomFact] //Code that allows us to display the random fact that has been picked from our array by previous code
     }
+    
+    //Also neet trick if facts dont fit inside label and display the "..." you can go to autoshrink in storyboard, and select min font scale and adjust the value below it allowing the text to an appropriate size to fit inside label.
 
 }
 
