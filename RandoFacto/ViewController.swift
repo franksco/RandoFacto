8cd59e75bb4859206564309e872b510e476a0732
franksco <franksco39@gmail.com>
2017-03-25 10:45:11 -0700

    created UI and now in the process of creating random fact gen

--- RandoFacto/ViewController.swift
+++ RandoFacto/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RandomFact(_ sender: Any) {
+        
+        let array = ["", "", "", "", "", "", "", "", "", "", "", ""]
+        
+        let RandomFact = Int(arc4random_uniform(UInt32(array.count)))
+        label.text = array[RandomFact]
+    }
 
 }
 
